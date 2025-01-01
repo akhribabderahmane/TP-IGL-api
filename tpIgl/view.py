daec08eb2777ae0f0fd58369c7d31d58aab6feaa
Islam Tobni <islamtobni@gmail.com>
2025-01-01 23:52:25 +0100

    consultation details added

--- tpIgl/view.py
+++ tpIgl/view.py
@@ -2,12 +2,19 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from users.serializers import LoginSerializer, UserSerializer
+from patients.serializers import PatientSerializer
+from patients.models import Patient
 
 @api_view(['POST'])
 def login(request):
     serializer = LoginSerializer(data=request.data)
     if serializer.is_valid():
         user = serializer.validated_data["user"]
-        response_data = UserSerializer(user).data
+        if user.role == 'patient':
+            patient = Patient.objects.get(user=user)
+            response_data = PatientSerializer(patient).data
+            response_data['user']['role'] = user.role
+        else:
+            response_data = UserSerializer(user).data
         return Response(response_data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
